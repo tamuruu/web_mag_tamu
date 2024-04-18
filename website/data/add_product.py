79b403f79323490940d6a88bdd6689b8c03fe0b5
SignumGusik <154827218+SignumGusik@users.noreply.github.com>
2024-04-18 16:13:05 +0300

    Add files via upload

---
+++ website/data/add_product.py
@@ -0,0 +1,18 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from datetime import datetime
+
+
+class AddProduct(SqlAlchemyBase):
+    __tablename__ = 'product'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    name = sqlalchemy.Column(sqlalchemy.String(80), nullable=False)
+    price = sqlalchemy.Column(sqlalchemy.Numeric(10, 2), nullable=False)
+    discount = sqlalchemy.Column(sqlalchemy.Integer, default=0)
+    stock = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    description = sqlalchemy.Column(sqlalchemy.Text, nullable=False)
+    pub_date = sqlalchemy.Column(sqlalchemy.DateTime, nullable=False, default=datetime.utcnow)
+
+    image1 = sqlalchemy.Column(sqlalchemy.String(150), nullable=False, default='image.jpg')
+    image2 = sqlalchemy.Column(sqlalchemy.String(150), nullable=False, default='image.jpg')
+    image3 = sqlalchemy.Column(sqlalchemy.String(150), nullable=False, default='image.jpg')
